19eb32a9179e07eeb533c4d854572ce01b0fe964
Mona Lisa <125455854+swervolee@users.noreply.github.com>
2023-09-29 22:40:37 +0300

    zombie

---
+++ 0x05-processes_and_signals/102-zombie.c
@@ -0,0 +1,45 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <sys/types.h>
+#include <sys/wait.h>
+#include <unistd.h>
+
+/**
+ *infinite_while - an infinite while loop
+ *Return: nothing
+ */
+int infinite_while(void)
+{
+	while (1)
+	{
+		sleep(1);
+	}
+	return (0);
+}
+
+
+/**
+ *main - entry point
+ *Return: nothing
+ */
+int main(void)
+{
+	pid_t f;
+	int i;
+
+	for (i = 0; i < 5; i++)
+	{
+		f = fork();
+
+		if (f > 0)
+		{
+			printf("Zombie process created, PID: %d\n", getpid());
+		}
+
+		else
+			exit(0);
+	}
+
+	infinite_while();
+	return (0);
+}
