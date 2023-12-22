14c7a176315f569a5744f635104828592d591169
Mona Lisa <125455854+swervolee@users.noreply.github.com>
2023-12-22 17:10:36 +0300

    correction

--- 0x0A-configuration_management/1-install_a_package.pp
+++ 0x0A-configuration_management/1-install_a_package.pp
@@ -1,5 +1,5 @@
 #installs Flask
-exec { 'install_flask':
-  command => '/usr/bin/pip3 install flask==2.1.0',
-  path    => '/usr/bin',
+package { 'flask':
+  ensure    => '2.1.0',
+  provider  => 'pip3'
 }
