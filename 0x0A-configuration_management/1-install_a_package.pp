602687d81a6afda236e220592ca06603472fa745
Mona Lisa <125455854+swervolee@users.noreply.github.com>
2023-12-22 15:00:04 +0300

    Installing Flask

---
+++ 0x0A-configuration_management/1-install_a_package.pp
@@ -0,0 +1,6 @@
+#installs Flask
+exec { 'install_flask':
+  command => '/usr/bin/pip3 install flask==2.1.0',
+  path    => '/usr/bin',
+  unless  => '/usr/bin/pip3 show flask | grep -q "Version: 2.1.0"',
+}
