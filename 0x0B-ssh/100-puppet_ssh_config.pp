1ef18fad8e88e40edd887fcbb883cbad0a221d8a
Mona Lisa <125455854+swervolee@users.noreply.github.com>
2024-01-01 23:55:08 +0300

    puppet ssh cofiguration

--- 0x0B-ssh/100-puppet_ssh_config.pp
+++ 0x0B-ssh/100-puppet_ssh_config.pp
@@ -1,6 +1,8 @@
+# ssh configuration
 include stdlib
 
 file_line { 'Turn off passwd auth':
+  ensure  => present,
   path    => '/etc/ssh/ssh_config',
   line    => '    PasswordAuthentication no',
   replace => true
@@ -8,6 +10,7 @@ file_line { 'Turn off passwd auth':
 
 
 file_line { 'Declare identity file':
+  ensure  => present,
   path    => '/etc/ssh/ssh_config',
   line    => '    IdentityFile ~/.ssh/school',
   replace => true
