2ca485720bf7415a72e17a76364863c153ff7b1a
Mona Lisa <125455854+swervolee@users.noreply.github.com>
2024-01-29 18:01:50 +0300

    Json correction

--- 0x15-api/2-export_to_JSON.py
+++ 0x15-api/2-export_to_JSON.py
@@ -8,7 +8,9 @@ if __name__ == '__main__':
     url = 'https://jsonplaceholder.typicode.com/'
     user = requests.get(f'{url}users/{sys.argv[1]}').json().get("username")
     todos = requests.get(f'{url}todos/', params={'userId': sys.argv[1]}).json()
-    data = {sys.argv[1]: [{"task": t.get("title"), "completed": t.get("completed"), "username": user} for t in todos]}
+    data = {sys.argv[1]: [{"task": t.get("title"),
+                           "completed": t.get(
+                               "completed"), "username": user} for t in todos]}
     filename = f"{sys.argv[1]}.json"
 
     with open(filename, 'w') as f:
