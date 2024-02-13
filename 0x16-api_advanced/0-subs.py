908aeddef2d1dae165af7daac830ab28a7a1b4b8
Mona Lisa <125455854+swervolee@users.noreply.github.com>
2024-02-13 16:11:33 +0300

    Innitial commit

---
+++ 0x16-api_advanced/0-subs.py
@@ -0,0 +1,21 @@
+#!/usr/bin/python3
+"""SUBREDDIT SUBSCRIBERS"""
+import requests
+
+
+def number_of_subscribers(subreddit=None):
+    """Function to retrieve the number of subscribers for a given subreddit"""
+
+    if not subreddit or not isinstance(subreddit, str):
+        return 0
+
+    try:
+        r = requests.get('http://reddit.com/r/{}/about.json'.
+                         format(subreddit),
+                         headers={'User-Agent': 'mycoolapp/1.0'})
+        r.raise_for_status()
+        data = r.json()
+        subscribers = data.get("data", {}).get("subscribers", 0)
+        return subscribers
+    except requests.RequestException as e:
+        return 0
